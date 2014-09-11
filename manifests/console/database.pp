0a3952f78f301c8186f0cb61dc915c00fcae4c7b
Josh Beard <beard@puppetlabs.com>
2014-09-11 16:05:39 -0400

    Manage the console_auth database settings
    The /etc/puppetlabs/console-auth/database.yml file should be managed by
    the pe_server::console::database class.

--- manifests/console/database.pp
+++ manifests/console/database.pp
@@ -1,15 +1,23 @@
 class pe_server::console::database (
   $password,
-  $database = 'console',
-  $username = 'console',
-  $host     = 'localhost',
-  $port     = '5432',
-  $adapter  = 'postgresql',
+  $console_auth_password,
+  $database              = 'console',
+  $username              = 'console',
+  $host                  = 'localhost',
+  $port                  = '5432',
+  $adapter               = 'postgresql',
+  $console_auth_database = 'console_auth',
+  $console_auth_username = 'console_auth',
 ) {
 
   file { '/etc/puppetlabs/puppet-dashboard/database.yml':
     ensure  => file,
     content => template('pe_server/database.yml.erb'),
+  }
+
+  file { '/etc/puppetlabs/console-auth/database.yml':
+    ensure  => file,
+    content => template('pe_server/console_auth_database.yml.erb'),
   }
 
 }
