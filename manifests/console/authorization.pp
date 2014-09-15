ff489d94106d54656c4bb5075c9d5abf610c98a4
Josh Beard <beard@puppetlabs.com>
2014-09-15 15:33:50 -0400

    Console auth refactoring
    We were using a defined type that used file_line for the console
    authorization.  However, the console auth spans multiple lines, so
    file_line was stupid for that.  This replaces the defined type with a
    class and expects a hash of authorizations.

--- manifests/console/authorization.pp
+++ manifests/console/authorization.pp
@@ -1,19 +1,12 @@
-define pe_server::console::authorization (
-  $certname = $name,
-  $match    = $name,
-  $role     = 'read-write',
+class pe_server::console::authorization (
+  $authorizations = {
+    'pe-internal' => {
+      'role'      => 'read-write'
+    },
+  },
 ) {
-
-  $console_auth = "${certname}:\n  role: ${role}\n"
-
-  file_line { "${certname}_console_authorization":
-    ensure => 'present',
-    #line  => inline_template('<%= @console_auth.to_yaml %>'),
-    line   => $console_auth,
-    match  => $match,
-    path   => '/etc/puppetlabs/console-auth/certificate_authorization.yml',
-    tag    => ['console_authorization'],
-    notify => Service['pe-httpd'],
+  file { '/etc/puppetlabs/console-auth/certificate_authorization.yml':
+    ensure  => 'file',
+    content => inline_template('<%= @authorizations.to_yaml %>'),
   }
-
 }
