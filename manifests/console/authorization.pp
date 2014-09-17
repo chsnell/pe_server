46ff8f39eeb3699ce7982757e7dd61167374f213
Josh Beard <beard@puppetlabs.com>
2014-09-17 00:36:48 -0400

    Restart pe-httpd when authorization changes

--- manifests/console/authorization.pp
+++ manifests/console/authorization.pp
@@ -8,5 +8,6 @@ class pe_server::console::authorization (
   file { '/etc/puppetlabs/console-auth/certificate_authorization.yml':
     ensure  => 'file',
     content => inline_template('<%= @authorizations.to_yaml %>'),
+    notify  => Service['pe-httpd'],
   }
 }
