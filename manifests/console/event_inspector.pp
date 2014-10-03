22b10afb74b1114a50154ab8f1474c222b3fa8f0
Josh Beard <beard@puppetlabs.com>
2014-10-03 15:57:13 -0600

    Better default certname for event inspector
    The event inspector default certname should be 'pe-internal-dashboard'
    
    Also add validation that the provided port is an integer.

--- manifests/console/event_inspector.pp
+++ manifests/console/event_inspector.pp
@@ -1,8 +1,12 @@
 class pe_server::console::event_inspector (
-  $console_cert_name  = $::fqdn,
+  $console_cert_name  = 'pe-internal-dashboard',
   $puppetdb_port      = '8081',
   $puppetdb_host      = $::fqdn,
 ) {
+
+  if !(is_integer($puppetdb_port)) {
+    fail('puppetdb_port must be an integer')
+  }
 
   file { 'pe_event_inspector_config':
     ensure  => file,
